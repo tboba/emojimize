5d1242324573354bd620d6224825d19d3d7db864
Tymoteusz Boba <Tymoteusz.Boba@gmail.com>
2021-03-06 16:41:03 +0100

    Apply requested changes, change the conditional statement to the filter.

--- src/main/java/pl/tymoteuszboba/emojimize/parser/EmojiParser.java
+++ src/main/java/pl/tymoteuszboba/emojimize/parser/EmojiParser.java
@@ -5,6 +5,7 @@ import pl.tymoteuszboba.emojimize.entity.emoji.Emoji;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,14 +16,14 @@ public class EmojiParser {
         String[] splitPhrase = originalString.split(" ");
 
         for (String word : splitPhrase) {
-            if (word.contains(":")) {
+            if (word.chars().filter(ch -> ch == ':').count() >= 2) {
                 Matcher emojiMatcher = Pattern.compile("(?:[:].*?[:])").matcher(word);
 
                 while (emojiMatcher.find()) {
-                    int firstEmojiIndex = emojiMatcher.start();
-                    int lastEmojiIndex = emojiMatcher.end();
+                    int emojiLeftBoundary = emojiMatcher.start();
+                    int emojiRightBoundary = emojiMatcher.end();
 
-                    String subStringEmoji = word.substring(firstEmojiIndex, lastEmojiIndex);
+                    String subStringEmoji = word.substring(emojiLeftBoundary, emojiRightBoundary);
                     String trimmedEmoji = subStringEmoji.replace(":", "");
 
                     try {
